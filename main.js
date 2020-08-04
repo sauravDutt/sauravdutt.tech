febcb097b9f8472cf9845a226294193f149ca5ed
sauravDutt <sauravd069@gmail.com>
2020-08-04 17:07:12 +0530

    First and Final Commit

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+$(document).ready(function () {
+    $('.menu-toggler').on('click', function(){
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+    });
+
+    $('.top-nav .nav-link').on('click', function(){
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+
+    AOS.init({
+        easin: 'ease',
+        duration: 1800
+    });
+
+});
+
+
+console.log("This Website is created with pure HTML5, CSS3 and JavaScript, No Fraworks, No Fancy Stuff !!");
+console.log("This website was builed in the Corona Virus Brakes(Quarantine)(28th March to the 9th of April, 2020)")
+console.log("for any quarries related to the website You can mail it to my personal email ID that is\nsauravd069@gmail.com")
+console.log("Thank you for visiting my Portfolio Website")
